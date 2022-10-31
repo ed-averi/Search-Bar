f0bd9f5d7eb8f2f5cfa2ec704a39c6a495ce1526
Gerson Vasquez <averienglish@aol.com>
2022-10-30 20:34:54 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+
+import { useMemo, useRef, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+const [items, setItems] =useState([ ])
+const [query, setQuery] =useState("")
+
+const inputRef= useRef()
+
+const filteredItems= useMemo(()=>{
+    return items.filter(item=>{
+    return item.toLowerCase().includes(query.toLowerCase())
+})
+},[items, query])
+
+function onSubmit(e){
+    e.preventDefault()// this prevents the form from refreshing the page
+    const value = inputRef.current.value // this access the input (line 24)
+    if (value === "") return
+    setItems(prev => {
+        return [... prev, value]
+    })
+    inputRef.current.value = "" // this deletes the input element value after it's added
+}
+return (<>
+    Search: 
+    <input value={query} onChange={e => setQuery(e.target.value)} type="search"/>
+    <br/>
+    <br/>
+    <form onSubmit={onSubmit}>
+        New Item: <input ref={inputRef} type="text"></input>
+        <button type='submit'>Add</button>
+    </form>
+    <h3>Items:</h3>
+    {filteredItems.map(item =>
+        <div>{item}</div>)}
+    </>
+
+    )
 }
 
 export default App;
